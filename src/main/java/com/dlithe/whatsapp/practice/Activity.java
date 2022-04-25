47ba55f7c075f6a385ff5c9c28484c25ea83144c
AmruthaAS18 <amruthaprinci@gmail.com>
2022-04-25 17:51:25 +0530

    "Adding some information in file and they is code for how to add two numbers"

---
+++ src/main/java/com/dlithe/whatsapp/practice/Activity.java
@@ -0,0 +1,20 @@
+/**
+ * This code to belong to dlithe company
+ */
+
+package com.dlithe.whatsapp.practice;
+
+
+/**
+ *
+ * @author Amrutha
+ * @version 0.1
+ * @since 25.04.22
+ */
+public class Activity {
+    public void addNumbers(){
+        int a=20,b=10;
+        int sum=a+b;
+        System.out.println(sum);
+    }
+}
